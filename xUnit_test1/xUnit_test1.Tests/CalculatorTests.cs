845f0bb2dfdd5f8f9f40e461221338b581caeb02
Краснов Илья <i.krasnov@bars-open.ru>
2016-07-18 19:37:10 +0500

    first app

---
+++ xUnit_test1/xUnit_test1.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace xUnit_test1.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void ShouldAdd()
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act 
+            var result = sut.Add(5, 5);
+
+            //Assert
+            Assert.Equal(10, result);
+
+        }
+    }
+}

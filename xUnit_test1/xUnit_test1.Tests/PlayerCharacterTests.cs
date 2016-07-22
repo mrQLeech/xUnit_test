e4663fc83740b149f862d44f995538f3cca4341c
QLeech <c-yleech@rambler.ru>
2016-07-22 10:15:14 +0500

    added classes to null and bool assertion

---
+++ xUnit_test1/xUnit_test1.Tests/PlayerCharacterTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace xUnit_test1.Tests
+{
+    class PlayerCharacterTests
+    {
+        [Fact]
+        public void ShouldHaveStartGeneratedName()
+        {
+            var player = new PlayerCharacter();
+            Assert.False(string.IsNullOrWhiteSpace(player.Name) || string.IsNullOrEmpty(player.Name));
+        }
+
+        [Fact]
+        public void ShouldNotHaveNickName()
+        {
+            var player = new PlayerCharacter();
+            Assert.Null(player.NickName);
+        }
+
+        [Fact]
+        public void ShouldNotBeNoob()
+        {
+            var player = new PlayerCharacter();
+            Assert.True(player.IsNoob);
+        }
+    }
+}
